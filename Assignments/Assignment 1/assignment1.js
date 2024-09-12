88704c94d3eacc7f3c315c4eec2c315078afd9b8
Yasas Maddumage <yasasrm@gmail.com>
2024-09-12 14:08:32 -0400

    Assignment1 - Header

--- Assignments/Assignment 1/assignment1.js
+++ Assignments/Assignment 1/assignment1.js
@@ -1,2 +1,9 @@
-console.log("Hello World!");
-
+/*********************************************************************************
+*  WEB700 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Yasas Maddumage Student ID: 170308233 Date: 2024/09/12
+*
+********************************************************************************/ 
