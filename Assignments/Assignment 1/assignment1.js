3f00386249dff57a6630d5bd4b6411c4f1d06d5e
Yasas Maddumage <yasasrm@gmail.com>
2024-09-12 14:42:34 -0400

    Assingment1 - httpRequest

--- Assignments/Assignment 1/assignment1.js
+++ Assignments/Assignment 1/assignment1.js
@@ -12,3 +12,15 @@ let serverVerbs = ["GET","GET","GET","POST","GET","POST"];
 let serverPaths = ["/","/about","/contact","/login","/panel","/logout"];
 let serverResponses = ["Welcome to WEB700 Assignment 1","This assignment was prepared by Yasas Maddumage","Yasas Maddumage: yrmaddumage@myseneca.ca","User Logged In","Main Panel","Logout Complete"];
 
+function httpRequest(httpVerb, path){
+    for (let i = 0; i < serverPaths.length; i++) {
+        if(serverVerbs[i] === httpVerb && serverPaths[i] === path){
+            return {200 : serverResponses[i]};
+        }
+    }
+    return {404 : `Unable to process ${httpVerb} request for ${path}`};
+}
+
+console.log(httpRequest("GET", "/"));
+console.log(httpRequest("GET", "/about"));
+console.log(httpRequest("PUT", "/")); 
