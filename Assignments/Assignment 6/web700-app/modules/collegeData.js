0a01635aa9f3dbcfd4318f1ead1c0f2ab2ac825f
Yasas Maddumage <yasasrm@gmail.com>
2024-12-05 16:35:45 -0500

    Assingment 6 -DB

---
+++ Assignments/Assignment 6/web700-app/modules/collegeData.js
@@ -0,0 +1,136 @@
+const Sequelize = require('sequelize');
+var sequelize = new Sequelize('neondb', 'neondb_owner', '1nabFStNgQ2q', {
+    host: 'ep-damp-forest-a5g96j25.us-east-2.aws.neon.tech',
+    dialect: 'postgres',
+    port: 5432,
+    dialectOptions: {
+        ssl: { rejectUnauthorized: false }
+    },
+    query: { raw: true }
+});
+
+const Student = sequelize.define('Student', {
+    studentNum: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    email: Sequelize.STRING,
+    addressStreet: Sequelize.STRING,
+    addressCity: Sequelize.STRING,
+    addressProvince: Sequelize.STRING,
+    TA: Sequelize.BOOLEAN,
+    status: Sequelize.STRING
+});
+
+const Course = sequelize.define('Course', {
+    courseId: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    courseCode: Sequelize.STRING,
+    courseDescription: Sequelize.STRING
+});
+
+Course.hasMany(Student, { foreignKey: 'course' });
+
+module.exports.initialize = () => {
+    return sequelize.sync()
+        .then(() => Promise.resolve())
+        .catch(err => Promise.reject("Unable to sync the database"));
+};
+
+module.exports.getAllStudents = () => {
+    return Student.findAll()
+        .then(data => Promise.resolve(data))
+        .catch(() => Promise.reject("No results returned"));
+};
+
+module.exports.getCourses = () => {
+    return Course.findAll()
+        .then(data => Promise.resolve(data))
+        .catch(() => Promise.reject("No results returned"));
+};
+
+module.exports.getCourseById = function(id) {
+    return new Promise((resolve, reject) => {
+        Course.findAll({ where: { courseId: id } })
+            .then(data => resolve(data[0]))
+            .catch(() => reject("no results returned"));
+    });
+};
+
+module.exports.getStudentByNum = function (num) {
+    return new Promise((resolve, reject) => {
+        Student.findAll({ where: { studentNum: num } })
+            .then(data => resolve(data[0]))
+            .catch(() => reject("no results returned"));
+    });
+};
+
+module.exports.getStudentsByCourse = function (course) {
+    return new Promise((resolve, reject) => {
+        Student.findAll({ where: { course } })
+            .then(data => resolve(data))
+            .catch(() => reject("no results returned"));
+    });
+};
+
+// Add student
+module.exports.addStudent = (studentData) => {
+    studentData.TA = studentData.TA ? true : false;
+    console.log(studentData);
+    for (const key in studentData) {
+        if (studentData[key] === "") studentData[key] = null;
+    }
+    return Student.create(studentData)
+        .then(() => Promise.resolve())
+        .catch(() => Promise.reject("Unable to create student"));
+};
+
+//Update student
+module.exports.updateStudent = function(studentData) {
+    studentData.TA = studentData.TA ? true : false;
+        for (let key in studentData) {
+            if (studentData[key] === "") {
+                studentData[key] = null;
+            }
+        }
+        return new Promise((resolve, reject) => {
+            Student.update(studentData, { where: { studentNum: studentData.studentNum } })
+                .then(() => resolve())
+                .catch(() => reject("unable to update student"));
+        });
+};
+
+//Delete course
+module.exports.deleteCourseById = (id) => {
+    return Course.destroy({ where: { courseId: id } })
+        .then(() => Promise.resolve())
+        .catch(() => Promise.reject("Unable to delete course"));
+};
+
+//Add course
+module.exports.addCourse = (courseData) => {
+    for (let key in courseData) {
+        if (courseData[key] === "") {
+            courseData[key] = null;
+        }
+    }
+    return new Promise((resolve, reject) => {
+        Course.create(courseData)
+            .then(() => resolve())
+            .catch(() => reject("unable to create course"));
+    });
+};
+
+//Update course
+module.exports.updateCourse = (courseData) => {
+    for (let key in courseData) {
+        if (courseData[key] === "") {
+            courseData[key] = null;
+        }
+    }
+    return new Promise((resolve, reject) => {
+        Course.update(courseData, { where: { courseId: courseData.courseId } })
+            .then(() => resolve())
+            .catch(() => reject("unable to update course"));
+    });
+};
+
+
---
+++ Assignments/Assignment 6/web700-app/server.js
@@ -0,0 +1,210 @@
+/*********************************************************************************
+*  WEB700 – Assignment 3
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Yasas Maddumage Student ID: 170308233 Date: 10/10/2024
+
+*  WEB700 – Assignment 4
+*  Name: Yasas Maddumage Student ID: 170308233 Date: 10/31/2024
+*
+*  WEB700 – Assignment 5
+*  Name: Yasas Maddumage Student ID: 170308233 Date: 11/26/2024
+*
+*  WEB700 – Assignment 6
+*  Name: Yasas Maddumage Student ID: 170308233 Date: 12/05/2024
+********************************************************************************/ 
+
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+const path = require('path');
+var app = express();
+const collegeData = require(path.join(__dirname, "modules", "collegeData"));
+const ejs = require("ejs"); 
+app.engine('ejs', ejs.renderFile); 
+app.set('view engine', 'ejs'); 
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts)
+app.set('layout', 'layouts/main');
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set "views" folder as the static directory for serving HTML files
+app.use(express.static(path.join(__dirname, "views")));
+
+// Body-parser middleware to handle form submissions
+app.use(express.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+    let route = req.path.substring(1);
+    app.locals.activeRoute =
+        "/" +
+        (isNaN(route.split("/")[1])
+            ? route.replace(/\/(?!.*)/, "")
+            : route.replace(/\/(.*)/, ""));
+    next();
+});
+
+app.locals.navLink = function (url, options) {
+    return (
+        '<li' +
+        (url === app.locals.activeRoute ? ' class="nav-item active"' : ' class="nav-item"') +
+        '><a class="nav-link" href="' +
+        url +
+        '">' +
+        options.fn(this) +
+        "</a></li>"
+    );
+};
+
+app.locals.equal = function (lvalue, rvalue, options) {
+    if (arguments.length < 3)
+        throw new Error("Ejs Helper 'equal' needs 2 parameters");
+    return lvalue !== rvalue ? options.inverse(this) : options.fn(this);
+};
+
+app.get('/', (req, res) => {
+    res.render('home', { title: "Home" }); 
+  });  
+  
+// Route to serve the about page
+app.get("/about", (req, res) => {
+    //res.sendFile("about.html", { root: "views" });
+    res.render('about');
+});
+
+// Route to serve the htmlDemo page
+app.get("/htmlDemo", (req, res) => {
+    //res.sendFile("htmlDemo.html", { root: "views" });
+    res.render('htmlDemo');
+});
+
+//Get Students
+app.get('/students', async (req, res) => {
+    const course = req.query.course;
+    try {
+        const students = course ? await getStudents(course) : await getStudents();
+        if (students.length > 0) {
+            res.render('students', { students });
+        } else {
+            res.render('students', {students});
+        }
+    } catch (err) {
+        res.render('students', {students});
+    }
+});
+
+//Get Student by id
+app.get("/student/:studentNum", (req, res) => {
+    const studentNum = req.params.studentNum;
+    Promise.all([
+        collegeData.getStudentByNum(studentNum),
+        collegeData.getCourses()                
+    ])
+    .then(([student, courses]) => {
+        res.render("student", { student, courses });
+    })
+    .catch((err) => {
+        console.log(err);
+        res.render("student", { message: "Student not found" });
+    });
+});
+
+
+//Update student
+app.post("/student/update", (req, res) => {
+    const updatedStudent = req.body;
+    collegeData.updateStudent(updatedStudent)
+        .then(() => {
+            res.redirect("/students");
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send("Unable to update student");
+        });
+});
+
+//Get Courses
+app.get("/courses", (req, res) => {
+    collegeData.getCourses()
+        .then((data) => {
+                res.render("courses", { courses: data });
+        })
+        .catch(() => {
+            res.render("courses", { courses: data });
+        });
+});
+
+//Get Course by id
+app.get("/course/:id", (req, res) => {
+    const courseId = req.params.id;
+
+    collegeData.getCourseById(courseId)
+        .then((course) => {
+            res.render("course", { course: course });
+        })
+        .catch((error) => {
+            res.render("course", { message: error });
+        });
+});
+
+const getStudents = async (course) => {
+    try {
+        if (course) {
+            return await collegeData.getStudentsByCourse(course);
+        } else {
+            return await collegeData.getAllStudents();
+        }
+    } catch (err) {
+        console.error(err);
+        throw new Error("No results");
+    }
+};
+
+app.get('/students/add', (req, res) => {
+    collegeData.getCourses()
+        .then((courses) => {
+            res.render("addStudent", { courses });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.render("addStudent", { message: "Error retrieving courses" });
+        });
+});
+
+app.post('/students/add', (req, res) => {
+    collegeData.addStudent(req.body)
+        .then(() => res.redirect('/students'))
+        .catch(err => res.status(500).send('Unable to add student: ' + err));
+});
+
+app.get('/courses/add', (req, res) => {
+    res.render('addCourse');
+});
+
+app.post('/courses/add', (req, res) => {
+    collegeData.addCourse(req.body)
+        .then(() => res.redirect('/courses'))
+        .catch(err => res.status(500).send('Unable to add course: ' + err));
+});
+
+// Other route handlers.
+app.use((req, res, next) => {
+    res.sendFile("error.html", { root: "views" });
+  });
+
+// Initialize the data and start the server
+collegeData.initialize()
+    .then(() => {
+        // setup http server to listen on HTTP_PORT
+        app.listen(HTTP_PORT, ()=>{console.log("server listening on port: " + HTTP_PORT)});
+    })
+    .catch(err => {
+        console.log("Failed to initialize data:", err);
+    });
+
+// Export the app for Vercel
+module.exports = app;
+
